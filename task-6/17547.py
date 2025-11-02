346d26df17d5d81a3fabb4c978c6cb66b2af9dea
di0mon <dimon070708@gmail.com>
2025-11-02 11:04:12 +0200

    .3

---
+++ task-6/17547.py
@@ -0,0 +1,30 @@
+from turtle import *
+screensize(2500,2500)
+tracer(False)
+m = 5
+
+for i in range(3):
+    fd(7*m)
+    rt(90)
+    fd(12*m)
+    rt(90)
+
+up()
+fd(4*m)
+rt(90)
+fd(6*m)
+lt(90)
+down()
+
+for i in range(4):
+    fd(83*m)
+    rt(90)
+    fd(77*m)
+    rt(90)
+up()
+for x in range(-10,15):
+    for y in range(-15,75):
+        goto(x*m,y*m)
+        dot(3,"orange")
+update()
+done()
