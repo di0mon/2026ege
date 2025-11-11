5591c3be8785c5273280ba38d6974df1c904bd9d
diomon <dimon070708@gmai.com>
2025-11-11 17:54:04 +0200

    задание 5

--- task-5/9774.py
+++ task-5/9774.py
@@ -1,8 +1,20 @@
+ans=[]
+from string import printable as alph
+def convert2(num, sus):
+    res = ""
+    while num != 0:
+        res += alph[num % sus]
+        num //= sus
+    return res[::-1]
+
 for N in range(1,100000):
-    from string import printable as alph
-    def convert2(N, 3):
-        res = " "
-        while N != 0:
-            res += alph[N % 3]
-            N //= 3
-        return res[::-1]
+    R = convert2(N, 3)
+    if N % 3 == 0:
+        R += R[-2:]
+    else:
+        R += convert2((N % 3) * 5, 3)
+    R = int(R, 3)
+    if R>133:
+        ans.append(R)
+print(min(ans))
+
