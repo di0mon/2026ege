19aa709a18f14cb091490df440a0c878a6491453
di0mon <dimon070708@gmail.com>
2025-11-21 23:49:12 +0200

    24.11

---
+++ task-14/9918.py
@@ -0,0 +1,8 @@
+
+for x in range(10,67):
+    for y in range(x):
+        num1=73*67**3 + x*67**2 + 1*67**1 + y*67**0
+        num2=int(f"49{y}6",x)
+        num=num1+num2
+        print(num)
+        break
