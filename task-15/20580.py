ca1475ef229b78b3bf707e342ef9e7bd2a9d6356
diomon <dimon070708@gmai.com>
2026-01-16 17:35:19 +0200

    практика 15 2 тип

---
+++ task-15/20580.py
@@ -0,0 +1,8 @@
+def f(x, y):
+    return (9 * x + y > A) or (x >= 36) or (y >= 18)
+
+
+for A in range(0, 1000):
+    if all(f(x, y) for x in range(1, 1000) for y in range(1, 1000)):
+        print(A)
+
